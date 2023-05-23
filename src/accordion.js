6f2283d611989ad40110211b50fbe7f9fdb5ee71
joydumply <nick.babichev@gmail.com>
2023-05-23 23:16:37 +0300

    fixed bugs

--- src/accordion.js
+++ src/accordion.js
@@ -1,29 +1,47 @@
 import {addLazyLoad} from './addLazyLoad.js';
 addLazyLoad();
-// console.log(addLazyLoad);
-// alert();
-
 const accordionItems = document.querySelectorAll('.accordion');
 
 accordionItems.forEach(item => {
     const title = item.querySelector('.accordion-title');
-    const content = item.querySelector('.accordion-content');
+    const btn = item.querySelector('.accordion-toggle-btn');
   
     title.addEventListener('click', () => {
-      const isOpen = item.classList.contains('open');
-  
-      closeAllAccordionItems();
-  
-      if (!isOpen) {
-        item.classList.add('open');
-        content.style.maxHeight = content.scrollHeight + 'px';
-      }
+      accordionOpen(item);
+    });
+
+    btn.addEventListener('click', () => {
+      accordionOpen(item);
     });
   });
 
-  function closeAllAccordionItems() {
-    accordionItems.forEach(item => {
-      item.classList.remove('open');
-      item.querySelector('.accordion-content').style.maxHeight = '0';
-    });
+  const accordionOpen = (item) => {
+    const content = item.querySelector('.accordion-content');
+    const isOpen = item.classList.contains('open');
+    const parent = item.closest('.accordion-wrap');
+    const openItem = parent.querySelector('.accordion.open');
+  
+      // closeAllAccordionItems();
+  
+      if (!isOpen) {
+        if(openItem){
+          accordionClose(openItem);
+        }
+
+        item.classList.add('open');
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }else{
+        accordionClose(item);
+      }
   }
+
+  const accordionClose = (item) => {
+    item.classList.remove('open');
+    item.querySelector('.accordion-content').style.maxHeight = '0';
+  }
+  // function closeAllAccordionItems() {
+  //   accordionItems.forEach(item => {
+  //     item.classList.remove('open');
+  //     item.querySelector('.accordion-content').style.maxHeight = '0';
+  //   });
+  // }
